9a0e58d36469b4a9e65543783f065673069ba176
DESKTOP-RD2LRN2\Elahe <elahe.fahmideh@gmail.com>
2026-06-21 16:26:50 +0330

    fix: add Image component config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.1.13'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '5000',
+        pathname: '/uploads/coverImage/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
