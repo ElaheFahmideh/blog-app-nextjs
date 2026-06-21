d47f9e55c1b764b65602bb94d69c73562349c8d9
DESKTOP-RD2LRN2\Elahe <elahe.fahmideh@gmail.com>
2026-06-21 22:01:04 +0330

    feat: handle create avatar component

---
+++ src/ui/avatar.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+
+type TAvatarProps = {
+  avatarUrl: string;
+  width?: NumberString;
+  height?: NumberString;
+};
+
+export default function Avatar({ avatarUrl, width = '25', height = '25' }: Readonly<TAvatarProps>) {
+  return (
+    <Image
+      className="rounded-full ring-1 ring-secondary-300"
+      src={avatarUrl}
+      alt={avatarUrl}
+      width={width}
+      height={height}
+      quality={90}
+      unoptimized
+    />
+  );
+}
